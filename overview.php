31fdf47616070b3b2af3f06ce81961eb9afd3b78
Harman Kang <hk22y@live.ca>
2016-05-07 14:24:43 -0400

    Update overview.php

--- overview.php
+++ overview.php
@@ -11,7 +11,7 @@
 $servername = "";
 $username = "";
 $password = "";
-$database = ""; */
+$database = "";
 
 	// create the connection
 	$connect = mysqli_connect($servername, $username, $password, $database);
@@ -30,7 +30,7 @@ $database = ""; */
 	<title>Stock Keeping System - Demo</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="../jumbovelvet/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
